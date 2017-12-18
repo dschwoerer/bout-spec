d142d36f6c1bd899b4616b72621c86042f966343
David Schwörer <schword2@mail.dcu.ie>
2017-12-18 17:23:24 +0000

    Add script to bump version

---
+++ update.sh
@@ -0,0 +1,24 @@
+# name of the spec file
+name=bout++-nightly
+# Author:
+who="David Schwörer <schword2mail.dcu.ie>"
+# Version to use
+vm="4.1.2-1"
+# Get the most recent full version
+version=$(git ls-remote https://github.com/dschwoerer/BOUT-dev.git|grep my_v4|cut -c 1-40)
+
+short=${version:0:7}
+if grep $version $name.spec &>/dev/null
+then
+    echo "up to date"
+    exit 0
+else
+    old=$(grep "%global commit" $name.spec)
+    sed "s/$old/%global commit $version/" $name.spec -i
+    rel=$(grep "Release:" $name.spec)
+    dat=$(date +%Y%m%d)
+    sed -i "s/$rel/Release:        $(date +%Y%m%d)git%{shortcommit}%{?dist}/" $name.spec
+    sed -i "s/%changelog/%changelog\n* $(date "+%a %b %d %Y") $who - $vm\n- Update to version $short\n/" $name.spec
+    #git diff
+    git commit -pm "Update $name to version $short"
+fi
